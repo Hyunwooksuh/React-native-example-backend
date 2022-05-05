dca52d9c257f3e47c5c1fc57173b862347ffaa92
hyunwook <shw@itemscout.io>
2022-05-05 16:02:03 +0900

    [Feat] 백엔드 서버 구축실습

---
+++ contactsapi/authentication/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from django.contrib import auth
+import jwt
+
+# Create your views here.
+class RegisterView (GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serilizer=UserSerializer(data=request.data)
+
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serilizer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(GenericAPIView):
+    def post(self, request):
+        data=request.data
+        username=data.get('username', '')
+        password=data.get('password', '')
+        user=auth.authenticate(username=username, password=password)
+
+        if user:
+            auth_token=jwt.encode({'username': user.username}, settings.JWT_SECRET_KEY)
+        
+            serilizer=UserSerializer(user)
+
+            data={
+                'user': serilizer.data,
+                'token': auth_token,
+            }
+
+            return Response(data, status=status.HTTP_200_OK)
+
+
+        return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
